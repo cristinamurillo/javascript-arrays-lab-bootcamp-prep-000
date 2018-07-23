4a9493c579a136e87be109a98a445e0e508aa3c2
Cristina Murillo <cmurill1@jhu.edu>
2018-07-23 23:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function destructivelyAppendKitten(name){
   return kittens
 }
 
-destructivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens
 }
